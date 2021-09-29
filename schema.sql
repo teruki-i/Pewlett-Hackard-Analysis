e45ee60a380fad7571d695965c71a870ebab5fe8
Teruki Ito <ito.teruki@gmail.com>
2021-09-28 18:52:15 -0700

    fixed errors in table setup

--- schema.sql
+++ schema.sql
@@ -36,13 +36,13 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE titles (
@@ -51,5 +51,4 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
+	PRIMARY KEY (emp_no, title, from_date)
